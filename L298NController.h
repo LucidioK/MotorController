858bbded335ffba622374b746b11ac68ac9ff0ca
lucidiok <lucidiok@hotmail.com>
2018-11-14 14:41:54 -0800

    Motor ahead/reverse

--- L298NController.h
+++ L298NController.h
@@ -4,19 +4,30 @@
 
 class L298NController {
 public:
-  
-  L298NController(int enApin, int in1pin, int enBpin, int in2pin) {
-    _enApin = enApin;
+
+  L298NController(int in1pin, int enApin, int in2pin, int csApin, int in3pin, int enBpin, int in4pin, int csBpin) {
     _in1pin = in1pin;
-    _enBpin = enBpin;
+    _enApin = enApin;
     _in2pin = in2pin;
+    _csApin = csApin;
+    _in3pin = in3pin;
+    _enBpin = enBpin;
+    _in4pin = in4pin;
+    _csBpin = csBpin;
+  }
+  
+  L298NController(int in1pin, int enApin, int in2pin, int csApin) {
+    initMinusOne();
+    _in1pin = in1pin;
+    _enApin = enApin;
+    _in2pin = in2pin;
+    _csApin = csApin;
   }
 
-  L298NController(int enApin, int in1pin) {
+  L298NController(int in1pin, int enApin) {
+    initMinusOne();
     _enApin = enApin;
     _in1pin = in1pin;
-    _enBpin = -1;
-    _in2pin = -1;
   }
 
   void Start(int motor) { Start(motor, 255); }
@@ -26,13 +37,15 @@ public:
   void Start(int motor, int power, int durationInMilliseconds) { Start(motor, power, durationInMilliseconds, true); }
 
   void Start(int motor, int power, int durationInMilliseconds, bool keepRunning) {
-    serialSSI("L298NController.Start", "motor", motor);
+    serialSSI("L298NController.Start-------------------------------", "motor", motor);
     serialSSI("L298NController.Start", "power", power);
     serialSSI("L298NController.Start", "durationInMilliseconds", durationInMilliseconds);
     serialSSI("L298NController.Start", "keepRunning", keepRunning ? 1 : 0);
-    digitalWriteInternal(getIN(motor), HIGH);
+    //serialWaitRead("--> BEFORE SENDING SIGNAL TO MOTOR: ");
+    digitalWriteInternal(getIN(motor, durationInMilliseconds < 0), HIGH);
+    //digitalWriteInternal(getIN(motor, durationInMilliseconds >= 0), LOW);
     analogWriteInternal (getEN(motor), power);
-    delay(durationInMilliseconds);
+    delay(abs(durationInMilliseconds));
     if (!keepRunning) {
       Stop(motor);
     }
@@ -40,12 +53,15 @@ public:
 
   void Stop(int motor) {
     serialSSI("L298NController.Stop", "motor", motor);
-    digitalWriteInternal(getIN(motor), LOW);
+    digitalWriteInternal(getIN(motor, true), LOW);
+    digitalWriteInternal(getIN(motor, false), LOW);
     analogWriteInternal (getEN(motor), 0);    
   }
 
 private:
   void digitalWriteInternal(int pin, int value) {
+    serialSSI("L298NController.digitalWriteInternal", "pin", pin);
+    serialSSI("L298NController.digitalWriteInternal", "value", value);
     if (pin >= 0) {
       digitalWrite(pin, value);
     }
@@ -56,8 +72,15 @@ private:
       analogWrite(pin, value);
     }
   }
+
+  void initMinusOne() { _in1pin = _enApin = _in2pin = _csApin = _in3pin = _enBpin = _in4pin = _csBpin = -1; };
   
-  int _enApin, _in1pin, _enBpin, _in2pin;
-  int getEN(int motor) { return motor == L298N_MOTOR_A ? _enApin : _enBpin; }
-  int getIN(int motor) { return motor == L298N_MOTOR_A ? _in1pin : _in2pin; }
+  int _in1pin, _enApin, _in2pin, _csApin, _in3pin, _enBpin, _in4pin, _csBpin;
+  int getEN(int motor) { return motor == L298N_MOTOR_A || _enBpin < 0 ? _enApin : _enBpin; }
+  int getIN(int motor, bool reverse) { 
+    return (motor == L298N_MOTOR_A  || _enBpin < 0) ? 
+      (reverse && _in2pin >= 0 ? _in2pin : _in1pin) : 
+      (reverse ? _in4pin : _in3pin); 
+  }
+
 };
--- motorController01.ino
+++ motorController01.ino
@@ -4,17 +4,23 @@
 #include "L298NController.h"
 #include <EEPROM.h>
 
-#define STATUS_SELECT        0
-#define STATUS_RUNNING_WATCH 1
-#define STATUS_RUNNING_MIX   2
+#define STATUS_SELECT         0
+#define STATUS_RUNNING_WATCH  1
+#define STATUS_RUNNING_MIX    2
+#define STATUS_RUNNING_RANDOM 3
+#define STATUS_RUNNING_AHEAD_REVERSE 4
+#define STATUS_RUNNING_REVERSE_ONLY 5
 
 #define STATUS_ADDRESS_ON_EEPROM 0
 
 String selectOperationMenu[] =
 {
-  "Which program v9", // STATUS_SELECT
+  "Which program vD", // STATUS_SELECT
   " WatchWnd",        // STATUS_RUNNING_WATCH
   " Mixer",           // STATUS_RUNNING_MIX
+  " Random",          // STATUS_RUNNING_RANDOM
+  " AheadRvrs",       // STATUS_RUNNING_AHEAD_REVERSE
+  " Reverse",         // STATUS_RUNNING_REVERSE_ONLY
   ""
 };
 
@@ -25,11 +31,28 @@ unsigned int  motorEngagementDurationInSeconds;
 unsigned int  motorPower0to255;
 bool keepRunning;
 bool firstRun;
-const int enA=11, in1=10;
+const int in1=2, enA=11, in2 = 12, csA = 13;
 unsigned long latestExecutionInSeconds;
-L298NController l298NController(enA, in1);
+int watchProgram[] = { 1000, 0, -1000, 0 };
+const int watchProgramCount = 4;
+int mixProgram[] = { 1000 };
+const int mixProgramCount = 1;
+int reverseProgram[] = { -1000 };
+const int reverseProgramCount = 1;
+int aheadReverseProgram[] = { 1000, 0, -1000, 0 };
+const int aheadReverseProgramCount = 4;
+int randomProgram[8];
+const int randomProgramCount = 8;
 
-void RunMotorIfNeeded() {
+
+L298NController l298NController(in1, enA, in2, csA);
+
+// Program is a sequence of time (in ms). 
+//  If a value is positive, it will run the motor.
+//  If a value is zero, it will stop the motor.
+//  If a value is negative, it will run the motor in reverse.
+// 
+void RunMotorIfNeeded(int *program, int programSize) {
   unsigned long currentSecond = millis() / 1000;
   
   if (currentSecond < latestExecutionInSeconds) { // millis will overflow after ~50 days...
@@ -45,15 +68,17 @@ void RunMotorIfNeeded() {
   
   if (firstRun || elapsedFromLatestExecution > intervalBetweenMotorEngagementsInSeconds) {
     MenuSelectorSimple::PrintAt("*", 15, 1);
-    l298NController.Start(L298N_MOTOR_A, motorPower0to255, motorEngagementDurationInSeconds * 1000, keepRunning);
+    for (int i =0; i < programSize; i++) {
+      if (program[i]) {
+        l298NController.Start(L298N_MOTOR_A, 255, program[i]);
+      } else {
+        l298NController.Stop(L298N_MOTOR_A);
+      }
+    }
     latestExecutionInSeconds = millis() / 1000;
     MenuSelectorSimple::PrintAt("_", 15, 1);
   }
-  
-  if (!keepRunning) {
-    digitalWrite(in1, LOW);
-  }
-  
+
   firstRun = false;
 }
 
@@ -64,11 +89,10 @@ void setup() {
   loopCount = intervalBetweenMotorEngagementsInSeconds = motorEngagementDurationInSeconds = motorPower0to255 = latestExecutionInSeconds = 0;
   keepRunning = false;
   Serial.begin(9600);
-  pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);  
-  digitalWrite(in1, LOW);
-  analogWrite(enA, 0);
+  for (int i = 4; i <= 13; i++) {
+    pinMode(i, OUTPUT);
+  }
+  randomSeed(analogRead(0));
 }
 
 
@@ -94,19 +118,39 @@ void loop() {
     if (MenuSelectorSimple::AnyButtonHit()) {
       EEPROM[STATUS_ADDRESS_ON_EEPROM] = STATUS_SELECT;
     }
-    if (currentStatus == STATUS_RUNNING_WATCH) {
-      intervalBetweenMotorEngagementsInSeconds = 15 * 60;
-      motorEngagementDurationInSeconds = 1;
-      motorPower0to255 = 128;
-      keepRunning = false;
+    
+    int* program;
+    int  programCount;
+
+    switch (currentStatus) {
+      case STATUS_RUNNING_WATCH:
+        intervalBetweenMotorEngagementsInSeconds = 5 * 60;
+        program = watchProgram;
+        programCount = watchProgramCount;
+        break;
+      case STATUS_RUNNING_MIX:
+        intervalBetweenMotorEngagementsInSeconds = 0;
+        program = mixProgram;
+        programCount = mixProgramCount;
+        break;
+      case STATUS_RUNNING_RANDOM:
+        intervalBetweenMotorEngagementsInSeconds = random(2, 10);
+        for (int i =0; i < randomProgramCount; i++) {
+          randomProgram[i] = (i % 2) ? 0 : 2000 - random(0, 4000);
+        }
+        program = randomProgram;
+        programCount = randomProgramCount;
+        break;
+      case STATUS_RUNNING_AHEAD_REVERSE:
+        program = aheadReverseProgram;
+        programCount = aheadReverseProgramCount;
+        break;
+      case STATUS_RUNNING_REVERSE_ONLY:
+        program = reverseProgram;
+        programCount = reverseProgramCount;
+        break;
     }
-    if (currentStatus == STATUS_RUNNING_MIX) {
-      intervalBetweenMotorEngagementsInSeconds = 0;
-      motorEngagementDurationInSeconds = 1;
-      motorPower0to255 = 255;
-      keepRunning = true;
-    }
-    RunMotorIfNeeded();
+    RunMotorIfNeeded(program, programCount);
   }
   previousStatus = currentStatus;
   delay(500);                       // wait for 1/2 seconds
